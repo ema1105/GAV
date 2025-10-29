8a4665168b7d3173e0fed00b0c36cb447d377330
Emmanuel <emanuelmorillopajaro@gmail.com>
2025-10-29 17:47:44 -0500

    Delete flutter_front directory

--- flutter_front/android/app/src/main/kotlin/com/example/flutter_front/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutter_front
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- flutter_front/lib/core/constants/api_constants.dart
+++
@@ -1,3 +0,0 @@
-class ApiConstants {
-  static const String baseUrl = 'http://localhost:8080/api'; 
-}
